bb1b6bb413521f378ad7fe01d3cac40ff33e9475
Leonard Rapp <leonardrapp@gmx.de>
2016-06-22 10:17:20 +0200

    implemented uart_readc function

--- main.c
+++ main.c
@@ -24,11 +24,31 @@ void uart_init() {
 	 UBRRL = UBRR_VAL & 0xFF;
 }
 
+uint8_t uart_readc() {
+	// wait until input is available
+    while (!(UCSRA & (1<<RXC)));
+    // read input from UDR and return
+    return UDR;
+}
+
 int main (void) {
 	// set pin 5 of PORTB for output
 	DDRB |= (1 << (5));
 
 	uart_init();
+
+	while(1) {
+		// If input char is available
+		if (UCSRA & (1<<RXC)) {
+			command = uart_readc();
+		}
+		// Check command
+		if (command == 1) {
+			PORTB == (0b00100000);
+		} else {
+			PORTB == (0b00000000);
+		}
+	}
 }
 
 
